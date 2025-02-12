0f1612b1b94c64101a64019a68d6def8ed8c7ed3
Данил Бабкин <93362757+proskovey@users.noreply.github.com>
2025-02-12 15:38:51 +0500

    Update settings.py

--- test_project/settings.py
+++ test_project/settings.py
@@ -26,10 +26,7 @@ SECRET_KEY = 'django-insecure-hbcp@70-o_g5hzf@$38@c%*h*(5%_o^fgi!h6qeckpppam=%6d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [
-    'www.proskovey.pythonanywhere.com',
-    'proskovey.pythonanywhere.com',
-]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
